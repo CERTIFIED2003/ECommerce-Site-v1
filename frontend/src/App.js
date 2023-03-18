32c0840c835d3ff119f73e1852dc6b0739895c9e
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-03-18 18:05:23 +0530

    Created Navbar and Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,55 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import "./App.scss";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <div style={{ marginBottom: "80px" }} />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+};
+
+const router = createBrowserRouter([
+  // {
+  //   path: "*",
+  //   element: <span>404 | Error</span>
+  // },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "*",
+        element: <span>404 | Error</span>
+      },
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      },
+    ]
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlined from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlined from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from 'react-router-dom';
+import "./Navbar.scss";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+
+                {/* LEFT */}
+                <div className="left">
+                    <div className="item">
+                        <img src="/assets/en.png" alt="english" />
+                        <KeyboardArrowDownIcon />
+                    </div>
+                    <div className="item">
+                        <span>USD</span>
+                        <KeyboardArrowDownIcon />
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products/women">Women</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products/men">Men</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/products/children">Children</Link>
+                    </div>
+                </div>
+
+                {/* CENTER */}
+                <div className="center">
+                    <Link className="link" to="/">CERTYSTORE</Link>
+                </div>
+
+                {/* RIGHT */}
+                <div className="right">
+                    <div className="item">
+                        <Link className="link" to="/">Homepage</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">About</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Contact</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Stores</Link>
+                    </div>
+                    <div className="icons">
+                        <SearchIcon />
+                        <PersonOutlineOutlined />
+                        <FavoriteBorderOutlined />
+                        <div className="cartIcon">
+                            <ShoppingCartOutlined />
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
