83c14d6bad8012e08980dc0bb976036a8dd10b0c
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-03-25 15:59:29 +0530

    Created Category Component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./Home.scss";
 import { Slider } from '../../components';
 import { FeaturedProducts } from "../../components";
+import Categories from '../../components/Categories/Categories';
 
 const Home = () => {
   return (
     <div className="home">
       <Slider />
       <FeaturedProducts type="Featured" />
+      <Categories />
       <FeaturedProducts type="Trending" />
     </div>
   )
