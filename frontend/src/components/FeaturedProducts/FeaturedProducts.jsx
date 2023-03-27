3a2cc8beb5e2c7eba5d5365c1564c191b1fcec55
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-03-28 00:30:06 +0530

    Created Hooks for fetching data

--- frontend/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Card from '../Card/Card';
 import "./FeaturedProducts.scss";
-import axios from "axios";
+import useFetch from '../../hooks/useFetch';
 
 const FeaturedProducts = ({ type }) => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await axios.get(`${process.env.REACT_APP_API_URL}/products?populate=*&[filters][type][$eq]=${type}`, {
-                    headers: {
-                        Authorization: `bearer ${process.env.REACT_APP_API_TOKEN}`
-                    }
-                });
-                setData(res.data.data);
-            }
-            catch (error) {
-                console.log(error);
-            }
-        };
-        fetchData();
-    }, [type]);
+    const { data, loading, error } = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`);
 
     return (
         <div className="featuredProducts">
@@ -34,9 +18,14 @@ const FeaturedProducts = ({ type }) => {
                 </p>
             </div>
             <div className="bottom">
-                {data.map((item) => (
-                    <Card key={item.id} item={item} />
-                ))}
+                {error
+                    ? "Something went wrong! Try again later..."
+                    : loading
+                        ? "loading"
+                        : data?.map((item) =>
+                            <Card key={item.id} item={item} />
+                        )
+                }
             </div>
         </div>
     )
